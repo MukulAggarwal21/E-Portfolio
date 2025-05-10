6d36c10a850647ce3bdb6d8a209c602e9ab4f395
MUKUL <mukul.2226csit1115@kiet.edu>
2025-05-11 02:32:18 +0530

    Hero Section and Navbar

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -3,19 +3,19 @@ import { Cursor, useTypewriter } from "react-simple-typewriter";
 import Media from './Media';
 
 const LeftBanner = () => {
-    const [text] = useTypewriter({
-      words: ["Youtuber.", "Software Engineer.", "DSA Enthusiast."],
-      loop: true,
-      typeSpeed: 20,
-      deleteSpeed: 10,
-      delaySpeed: 2000,
-    });
+  const [text] = useTypewriter({
+    words: ["Full Stack Developer.", "Open Source Contributor.", "Freelancer.", "Tech Enthusiast."],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">Shaurya Awasthi</span>
+          Hi, I'm <span className="text-designColor capitalize">Mukul Aggarwal</span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           <span>{text}</span>
@@ -26,11 +26,10 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-        I'm a  BTech 2024 graduate and an upcoming Advanced Associate Software Engineer @Accenture. With a strong foundation in DSA and a passion for coding, I aim to share valuable insights, tutorials, and experiences from my journey. 
-        </p>
+          As a Full-Stack Developer, I specialize in React.js, Next.js, JavaScript, Node.js, and Tailwind CSS to create intuitive and responsive applications. I ensure optimized and efficient code for scalable solutions.        </p>
       </div>
-     {/* Media */}
-     <Media />
+      {/* Media */}
+      <Media />
     </div>
   );
 }
--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -2,46 +2,54 @@ import React from 'react';
 import { BsGithub } from "react-icons/bs";
 import { FaInstagram, FaLinkedinIn, FaReact, FaYoutube } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { RiHtml5Fill } from "react-icons/ri";
+// import { RiHtml5Fill } from "react-icons/ri";
+import { IoLogoNodejs } from "react-icons/io5";
+import { SiNextdotjs } from "react-icons/si";
+
 const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
+      <div>
+        <h2 className="text-base uppercase font-titleFont mb-4">
           Find me in
-                </h2>
-                <div className="flex gap-4">
-                  <a href="https://github.com/Shauryaa007"><span className="bannerIcon">
-                    <BsGithub/>
-                  </span></a>
-                  <a href="https://www.youtube.com/@Shauryaaa007"><span className="bannerIcon">
-                  <FaYoutube />
-                  </span></a>
-                  <a href="https://www.linkedin.com/in/shaurya007/"><span className="bannerIcon">
-                    <FaLinkedinIn />
-                  </span></a>
-                  <a href="https://www.instagram.com/shauryaa.cpp/"><span className="bannerIcon">
-                    <FaInstagram />
-                  </span></a>
-          </div>
-        </div>
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            BEST SKILL ON
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaReact />
-            </span>
-            <span className="bannerIcon">
-              <RiHtml5Fill />
-            </span>
-            <span className="bannerIcon">
-              <IoLogoJavascript/>
-            </span>
-          </div>
+        </h2>
+        <div className="flex gap-4">
+          <a href="https://github.com/MukulAggarwal21https://github.com/MukulAggarwal21"><span className="bannerIcon">
+            <BsGithub />
+          </span></a>
+
+          <a href="https://www.linkedin.com/in/mukul-aggarwal-850368261/"><span className="bannerIcon">
+            <FaLinkedinIn />
+          </span></a>
+          <a href="https://www.instagram.com/mukul_aggarwal21_/"><span className="bannerIcon">
+            <FaInstagram />
+          </span></a>
         </div>
       </div>
+      <div>
+        <h2 className="text-base uppercase font-titleFont mb-4">
+          BEST SKILL ON
+        </h2>
+        <div className="flex gap-4">
+          <span className="bannerIcon">
+            <FaReact />
+          </span>
+          <span className="bannerIcon">
+            {/* <RiHtml5Fill /> */}
+            <IoLogoNodejs />
+
+          </span>
+          <span className="bannerIcon">
+            <IoLogoJavascript />
+
+          </span>
+          <span className="bannerIcon">
+            <SiNextdotjs />
+
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
