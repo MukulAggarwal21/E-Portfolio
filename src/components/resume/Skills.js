560025cc6b8685ee8657e5bc5a7bb7da5fd11b18
MUKUL <mukul.2226csit1115@kiet.edu>
2025-05-15 00:10:14 +0530

    Skills

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -15,66 +15,96 @@ import {
   SiRedis,
   SiNextdotjs,
   SiPostgresql,
-  SiDjango,
+  // SiDjango,
   SiTypescript,
-  SiGo,
+  // SiGo,
   SiVisualstudiocode,
   SiPostman,
   SiSlack,
   SiVercel,
-  SiLinux,
+  // SiLinux,
   SiGit,
-  SiHeroku,
-  SiDocker,
+
   SiGithub,
   SiNotion,
   SiNetlify,
+  SiNodemon,
+  SiTailwindcss,
+  SiRedux,
+  SiIntellijidea,
+  SiJira ,
+  SiRailway ,
+  // SiRender ,
 } from "react-icons/si";
+import { FaCss3, FaAws, FaBootstrap } from "react-icons/fa";
+import { SiSocketdotio } from "react-icons/si";
+import { DiNginx } from "react-icons/di";
+import { IoLogoFirebase } from "react-icons/io5";
+
+import { FaHtml5 } from "react-icons/fa";
 
 export default function ProfessionalSkillset() {
   const [hoveredSkill, setHoveredSkill] = useState(null);
   const [hoveredTool, setHoveredTool] = useState(null);
-  
+
   const skills = [
-    { id: 'cpp', name: 'C++', icon: CgCPlusPlus },
     { id: 'js', name: 'JavaScript', icon: DiJavascript1 },
     { id: 'ts', name: 'TypeScript', icon: SiTypescript },
-    { id: 'node', name: 'Node.js', icon: DiNodejs },
     { id: 'react', name: 'React', icon: DiReact },
     { id: 'next', name: 'Next.js', icon: SiNextdotjs },
-    { id: 'django', name: 'Django', icon: SiDjango },
+    { id: 'node', name: 'Node.js', icon: DiNodejs },
+    { id: 'redux', name: 'Redux', icon: SiRedux },
+
+    { id: 'html', name: 'HTML5', icon: FaHtml5 },
+    { id: 'css', name: 'CSS', icon: FaCss3 },
+    { id: 'tailwind', name: 'Tailwind', icon: SiTailwindcss },
+    { id: 'bootstrap', name: 'Bootstrap', icon: FaBootstrap },
+    // { id: 'django', name: 'Django', icon: SiDjango },
     { id: 'git', name: 'Git', icon: DiGit },
     { id: 'mongo', name: 'MongoDB', icon: DiMongodb },
+
+    { id: 'firebase', name: 'Firebase', icon: IoLogoFirebase },
     { id: 'postgres', name: 'PostgreSQL', icon: SiPostgresql },
     { id: 'mysql', name: 'MySQL', icon: DiMysql },
+    { id: 'nodemon', name: 'Nodemon', icon: SiNodemon },
+
     { id: 'redis', name: 'Redis', icon: SiRedis },
+    { id: 'cpp', name: 'C++', icon: CgCPlusPlus },
+
     { id: 'python', name: 'Python', icon: DiPython },
     { id: 'java', name: 'Java', icon: DiJava },
-    { id: 'go', name: 'Go', icon: SiGo },
+    // { id: 'go', name: 'Go', icon: SiGo },
+    { id: 'aws', name: 'AWS', icon: FaAws },
+    { id: 'nginx', name: 'Nginx', icon: DiNginx },
+    { id: 'socket', name: 'Socket.IO', icon: SiSocketdotio },
   ];
-  
+
   const tools = [
-    { id: 'linux', name: 'Linux', icon: SiLinux },
     { id: 'vscode', name: 'VS Code', icon: SiVisualstudiocode },
     { id: 'postman', name: 'Postman', icon: SiPostman },
+        { id: 'railway', name: 'Railway', icon: SiRailway },
+
     { id: 'slack', name: 'Slack', icon: SiSlack },
     { id: 'vercel', name: 'Vercel', icon: SiVercel },
     { id: 'netlify', name: 'Netlify', icon: SiNetlify },
-    { id: 'heroku', name: 'Heroku', icon: SiHeroku },
-    { id: 'docker', name: 'Docker', icon: SiDocker },
+    // { id: 'render', name: 'Render', icon: SiRender },
+
     { id: 'github', name: 'GitHub', icon: SiGithub },
+    { id: 'IntelliJ', name: 'IntelliJ', icon: SiIntellijidea },
+    { id: 'Jira', name: 'Jira', icon: SiJira },
+
     { id: 'notion', name: 'Notion', icon: SiNotion },
   ];
 
   const renderSkillIcon = (skill) => {
-    return React.createElement(skill.icon, { 
-      className: "text-5xl md:text-6xl text-white" 
+    return React.createElement(skill.icon, {
+      className: "text-5xl md:text-6xl text-white"
     });
   };
 
   const renderToolIcon = (tool) => {
-    return React.createElement(tool.icon, { 
-      className: "text-5xl md:text-6xl text-white" 
+    return React.createElement(tool.icon, {
+      className: "text-5xl md:text-6xl text-white"
     });
   };
 
@@ -83,7 +113,7 @@ export default function ProfessionalSkillset() {
       {/* Stars background effect */}
       <div className="fixed inset-0 z-0 pointer-events-none">
         {[...Array(100)].map((_, i) => (
-          <div 
+          <div
             key={i}
             className="absolute rounded-full bg-white"
             style={{
@@ -124,13 +154,13 @@ export default function ProfessionalSkillset() {
             ))}
           </div>
         </div>
-        
+
         {/* Tools Section */}
         <div>
           <h1 className="text-4xl md:text-5xl font-bold text-center mb-12">
             <span className="text-purple-400">Tools</span> I Use
           </h1>
-          
+
           <div className="grid grid-cols-3 md:grid-cols-5 gap-4">
             {tools.map((tool) => (
               <div
